9ec8d55e381c53b816a5aecb9f9fc526c4ba0a59
Sérgio <sergio@even3.com>
2025-06-07 10:45:40 -0300

    Adiciona estrutura basica do spring e suas dependencias

---
+++ src/main/java/br/com/ifpe/teampulse/TeampulseApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.teampulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeampulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeampulseApplication.class, args);
+	}
+
+}
