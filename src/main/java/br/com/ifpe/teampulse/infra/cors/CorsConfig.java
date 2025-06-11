47adac592fc68dc2c94a4953aa4327f433f6ab4e
ocesar9 <juliocesaoguimaraes@gmail.com>
2025-06-10 22:30:58 -0300

    Cors Config

---
+++ src/main/java/br/com/ifpe/teampulse/infra/cors/CorsConfig.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.teampulse.infra.cors;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
