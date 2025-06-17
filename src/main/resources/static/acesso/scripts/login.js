91474a28d03e0add8a4ef538c5fa51a1d8c934d1
Sérgio <sergio@even3.com>
2025-06-17 18:59:12 -0300

    adiciona validação e exibição de erro + finaliza fluxo de login com redirecionamento + troca busca de elementos por id para data-attributes

--- src/main/resources/static/acesso/scripts/login.js
+++ src/main/resources/static/acesso/scripts/login.js
@@ -1,28 +1,45 @@
 document.getElementById('form-login').addEventListener("submit", async (e) => {
     e.preventDefault();
 
+    const btnLogin = document.querySelector("[data-login-btn]")
+    const btnSpinner = document.querySelector("[data-login-spinner]")
+    const btnTexto = document.querySelector("[data-login-text]")
+    const msgError = document.querySelector("[data-login-error-list]")
+
+    btnLogin.disabled = true;
+    btnTexto.textContent = "";
+    btnSpinner.classList.remove("d-none");
+    msgError.classList.add("d-none");
+
     const form = e.target;
     const formData = new FormData(form);
     const dados = Object.fromEntries(formData.entries())
 
-    const response = await fetch("http://localhost:8080/auth/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(dados)
-    });
+    try {
+        const response = await fetch("http://localhost:8080/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dados)
+        });
 
 
-    if (response.ok) {
-        const btn = document.getElementById("js-btn-logar");
-        btn.textContent = "Loading..."
-        setTimeout(() => {
-            window.location = "http://localhost:8080/content/dashboard"
-        }, 1000)
+        if (response.ok) {
+            setTimeout(() => {
+                window.location = "http://localhost:8080/content/dashboard"
+            }, 1000)
+        }
+        else {
+            btnLogin.disabled = false;
+            btnTexto.textContent = "Entrar";
+            btnSpinner.classList.add("d-none");
+            msgError.classList.remove("d-none");
+            msgError.textContent = "Erro. Tente novamente"
+        }
+        console.log(response)
     }
-    else {
-        alert("erro")
+    catch (error) {
+        console.log(error)
     }
-
 })
